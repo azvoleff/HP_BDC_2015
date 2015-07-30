4a2b05323c691bca8ba6c5dc509b8f764fba708e
Alex Zvoleff <azvoleff@gmail.com>
2015-07-30 16:17:59 +0200

    Add demo and clean code for full run.

--- 3_run_models.R
+++ 3_run_models.R
@@ -8,11 +8,14 @@ pred_cols <- list("r1", "r2", "r3", "r4", "r5", "r7", "veg", "vegmean",
                   "vegvar", "vegdis", "elev", "slop", "asp", "datayear")
 
 distributedR_start()
+con <- odbcConnect("ctf")
 
 sites <- read.csv('/home/radmin/HP_BDC_2015/sitecode_key.csv')
 sitecodes <- sites$sitecode
 
 foreach (sitecode=sitecodes) %do% {
+    message(date(), " *** Processing ", sitecode, "***")
+
     message(date(), ": Loading training data")
     train_data <- db2dframe(paste0("cit.", sitecode, "_training"), 'ctf',
                             features=c(pred_cols, "type"))
@@ -24,20 +27,24 @@ foreach (sitecode=sitecodes) %do% {
     message(date(), ": Finished training random forest model")
 
     message(date(), ": Deploying model to vertica db")
+    sqlQuery(con, paste0("DELETE FROM R_models WHERE model = ( 
+        SELECT DeleteModel(USING PARAMETERS model = 'dbadmin/", sitecode,
+        "_rfmodel') over());"))
     deploy.model(model=rfmodel, dsn='ctf', modelName=paste0(sitecode, '_rfmodel'),
-                modelComments=paste('Random forest model for', sitecode))
+                 modelComments=paste('Random forest model for', sitecode))
     message(date(), ": Finished deploying model to vertica db")
 
-    # message(date(), ": Running in-database prediction in vertica")
-    con <- odbcConnect("ctf")
+    message(date(), ": Running in-database prediction in vertica")
     sqlQuery(con, paste0('DROP TABLE cit.', sitecode, '_prediction;'))
     sqlQuery(con, paste0('CREATE TABLE cit.', sitecode, '_prediction (type varchar);'))
     apply_sql <- paste0("INSERT INTO cit.", sitecode, "_prediction", 
                         " (SELECT randomForestpredict(",
                         paste(pred_cols, collapse=", "),
-                        " USING PARAMETERS model='dbadmin/", sitecode,
+                        " USING PARAMETERS model='", sitecode,
                         "_rfmodel') FROM cit.", sitecode,
-                        "_predictor WHERE datayear = 2010);")
+                        "_predictor WHERE datayear = 2000);")
     sqlQuery(con, apply_sql)
     message(date(), ": Finished running in-database prediction in vertica")
+
+    message(date(), " *** Finished ", sitecode, "***")
 }
---
+++ demo.R
@@ -0,0 +1,35 @@
+library(vRODBC) # for SQL connection to Vertica database
+library(distributedR) # for hpdRF_parallelTree, deploy.model
+library(HPdclassifier) # for hpdrandomForest
+library(HPdata) # for db2dframe
+
+pred_cols <- list("r1", "r2", "r3", "r4", "r5", "r7", "veg", "vegmean", 
+                  "vegvar", "vegdis", "elev", "slop", "asp", "datayear")
+
+distributedR_start()
+con <- odbcConnect("ctf")
+
+message(date(), ": Training random forest model")
+train_data <- db2dframe("cit.PSH_training", 'ctf', features=c(pred_cols, "type"))
+
+rfmodel <- hpdRF_parallelTree(type ~ ., data=train_data, na.action=na.omit,
+                              ntree=1000)
+message(date(), ": Finished training random forest model")
+
+message(date(), ": Running in-database prediction in vertica")
+
+# Deploy model to Vertica DB
+sqlQuery(con, "DELETE FROM R_models WHERE model = (SELECT DeleteModel(USING PARAMETERS model = 'dbadmin/PSH_rfmodel') over());")
+
+deploy.model(model=model, dsn='ctf', modelName='PSH_rfmodel', modelComments='Random forest model for Pasoh')
+
+# Create table to hold predictions
+sqlQuery(con, 'DROP TABLE cit.PSH_prediction;')
+sqlQuery(con, 'CREATE TABLE cit.PSH_prediction (type varchar);')
+
+# Run in-database prediction
+sqlQuery(con, paste0("INSERT INTO cit.PSH_prediction (SELECT randomForestpredict(",
+                     paste(pred_cols, collapse=", "),
+                     " USING PARAMETERS model='PSH_rfmodel') FROM cit.PSH_predictor WHERE datayear = 2010);")
+
+message(date(), ": Finished running in-database prediction in vertica")
